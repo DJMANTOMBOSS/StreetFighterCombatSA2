fa6686ff3a9e8d475c3634345192c31f260a6df0
DJMANTOMBOSS <126653942+DJMANTOMBOSS@users.noreply.github.com>
2024-12-30 00:35:10 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to Street Fighter: Combat SA</h1>
+            <button>Play Now</button>
+        </div>
+    );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Street Fighter: Combat SA
+
+This is a fully functional web-based gaming project, integrating front-end and back-end features.
